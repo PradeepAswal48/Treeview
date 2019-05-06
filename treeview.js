e954256233e012dbbfa7f4636b8f36242861a38b
PradeepAswal48 <35682978+PradeepAswal48@users.noreply.github.com>
2019-05-06 09:29:03 +0530

    Update treeview.js

--- treeview.js
+++ treeview.js
@@ -3,8 +3,14 @@ import { html } from 'lit-html/lit-html'
 class Treeview extends LitElement {
   static get properties(){
     return{
+      type:{
+        type:String
+      },
+      id:{
+        type:String
+      },
       objectType :{
-        type:Array
+        type:Object
       },
       data : {
         type : Object
@@ -20,13 +26,13 @@ class Treeview extends LitElement {
 
   }
   hasProperty(item){
-    if(this.objectType.indexOf(item)>0){
+    if(this.objectType[item]){
       return true;
     }
     return false;
   }
-  parseJSONtoHTML(JSONObject){
-    
+  parseJSONtoHTML(){
+    var JSONObject = this.data;
     for( var item in JSONObject){
       if(this.hasProperty(item)){
         this.node.push(JSONObject[item]);
@@ -44,39 +50,74 @@ class Treeview extends LitElement {
     super();
     this.node = [];
     this.leafNode = [];
-    this.objectType = ['Simulation', 'Activities'];
+    this.loadObjectTypes();
+    
 
   }
+  connectedCallback(){
+    super.connectedCallback();
+    console.log('i load before render');
+    this.innerText='loading';
+  }
+  fetchData(){
+    console.log("Data load for"+ this.type);
+    if(this.type === 'simulation'){
+      this.data = {'name':'simulation1',
+                    'type':'simulation',
+                    'activity':{'name':'Activty Name',
+                    'id':'act-123',
+                  'type':'activity'}};
+      
+    }
+    else if(this.type ==='activity'){
+      this.data = {'name':'Act1',
+    'executionOption':{'name':'Execution Option','type':'executionOption',
+  'id':'exe-123'}};
+    }
+    
+  }
+  loadObjectTypes() {
+    var instance = this;
+    var request = new XMLHttpRequest();
+    request.open('GET',"./treeDefination.json", false);
+    request.onreadystatechange = function() {
+      if (request.readyState === 4) {
+        instance.objectType=JSON.parse(request.response);
+      }
+    }
+    request.send();
+  }
+ 
   firstUpdated(changedProperties) {
     var toggler = this.renderRoot.querySelectorAll('.more');
     var i;
-
     for (i = 0; i < toggler.length; i++) {
       toggler[i].addEventListener("click", function () {
-        this.parentElement.querySelector(".nested").classList.toggle("active");
+        var id=this.getAttribute('data-id');
+        var type=this.getAttribute('data-type');
+        if(! this.parentElement.querySelector('#'+id)){
+          this.parentElement.querySelector(".nested").innerHTML += "<tree-view id="+id+" type="+type+"></tree-view>";
+        }
+         this.parentElement.querySelector(".nested").classList.toggle("active");
         this.classList.toggle("more-down");
+        
+        
       });
     }
 
   }
   render() {
-    let temp = {'attribute6':'value6','Activities':'will have more'};
-    this.parseJSONtoHTML(temp);
+    this.fetchData();
+    this.parseJSONtoHTML();
     return html`
-    <link rel="stylesheet" href="./treeview.css">
-    <div class="processTree">
-    <span>Process Browser</span>
-    <div>
+    <link rel="stylesheet" href="./treeview-css.css">
+    
     <ul id="myContent">
-    
-    ${this.node.map(nodes => html`<li>
-    <span class="more">${nodes}<span></li>
-    `)}
-    
     ${this.leafNode.map(leafnodes => html`<li>${leafnodes}</li>`)}
+    ${this.node.map(nodes => html`<li>
+    <span data-id='${nodes.id}' data-type='${nodes.type}' class="more">${nodes.name}</span><ul class='nested'></ul></li>
+    `)}
   </ul>
-    </div>
-    </div>
     
     `;
   }
